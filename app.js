eb58233a200af670b526b995d1739ed4689eb735
Roshan <roshanbhuvad15@gmail.com>
2020-08-07 10:25:54 +0530

    Database connection done

--- app.js
+++ app.js
@@ -5,4 +5,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Setup Error handlers
+const errorHandlers = require("./handlers/errorHandlers");
+app.use(errorHandlers.notFound);
+app.use(errorHandlers.mongooseErrors);
+if (process.env.ENV === "DEVELOPMENT") {
+  app.use(errorHandlers.developmentErrors);
+} else {
+  app.use(errorHandlers.productionError);
+}
 module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Mongoose Connection Error: " + err.message);
+});
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connected!");
+});
 const app = require("./app");
 
-app.listen(800, () => {
+app.listen(8000, () => {
   console.log("Server listening on port 8000");
 });
